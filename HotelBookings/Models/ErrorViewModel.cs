e6a8fff69848f3e309a9cd09fad060d002c4fd44
Trung Hiếu Trương <kimthu86zz@gmail.com>
2024-09-03 10:01:28 +0700

    Add project files.

---
+++ HotelBookings/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HotelBookings.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
